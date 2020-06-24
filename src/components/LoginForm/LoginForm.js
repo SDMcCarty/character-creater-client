3f2b8f820e71eb10e4c85d32610f418f3392b0d2
Sherry McCarty <sherrydmccarty@gmail.com>
2020-06-24 12:24:55 -0400

    setting up login functionality

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,28 +1,51 @@
 import React, { Component } from 'react';
+import TokenService from '../../services/token-service';
+import AuthApiService from '../../services/auth-api-service';
 import { Link } from 'react-router-dom';
 
 class LoginForm extends Component {
+  static defaultProps = {
+    onLoginSuccess: () => {}
+  }
+
   state = { error: null}
-  handleSubmit = (e) => {
+
+  handleSubmitJwtAuth = (e) => {
     e.preventDefault()
+    this.setState({ error: null })
+    const { user_name, password } = e.target
+
+    AuthApiService.postLogin({
+      user_name: user_name.value,
+      password: password.value
+    })
+      .then(res => {
+        user_name.value = ''
+        password.value = ''
+        TokenService.saveAuthToken(res.authToken)
+        this.props.onLoginSuccess()
+      })
+      .catch(res => {
+        this.setState({ error: res.error })
+      })
 
   }
 
   render() {
     const { error } = this.state
     return (
-      <form className='LoginForm' onSubmit={this.handleSubmit}>
+      <form className='LoginForm' onSubmit={this.handleSubmitJwtAuth}>
+        <div role='alert'>
+          {error && <p className='red'>{error}</p>}
+        </div>
         <fieldset>
           <legend>Log In</legend>
-            <div role='alert'>
-              {error && <p className='red'>{error}</p>}
-            </div>
-          <label htmlFor='LoginForm__username'>User Name</label>
-          <input required type='text' name='username' id='LoginForm__username' />
+          <label htmlFor='LoginForm__user_name'>User Name</label>
+          <input required type='text' name='user_name' id='LoginForm__user_name' />
           <label htmlFor='LoginForm__password'>Password</label>
           <input required type='password' name='password' id='LoginForm__password' />
           <Link to='/register'>Not a member? Signup now!</Link>
-          <button type='submit'>Sign In</button>
+          <button type='submit'>Log In</button>
           {/* <Link to='/'><button type='button'>Cancel</button></Link> */}
         </fieldset>
       </form>
---
+++ src/config.js
@@ -0,0 +1,4 @@
+export default {
+  API_ENPOINT: 'http://localhost:8000/api',
+  TOKEN_KEY: 'character-creater-api-auth-token',
+}
