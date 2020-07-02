398330aedd5ede3160782d9d1d1cf58e337cdd43
Sherry McCarty <sherrydmccarty@gmail.com>
2020-07-02 10:30:04 -0400

    edited test

--- src/components/CharacterList/CharacterList.test.js
+++ src/components/CharacterList/CharacterList.test.js
@@ -1,24 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 // import { shallow } from 'enzyme';
 // import toJson from 'enzyme-to-json';
 import CharacterList from './CharacterList';
 
 describe('Character List', () => {
-  // const props = {
-  //   first_name: 'A',
-  //   last_name: 'B',
-  //   age: '34',
-  //   sex: 'female',
-  //   major_trait: 'brazen',
-  //   motivation: 'brains',
-  //   fear: 'fire',
-  //   history: 'everything was fine until the bite',
-  // }
 
   it('renders the character list', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<CharacterList />, div);
+    ReactDOM.render(<BrowserRouter><CharacterList /></BrowserRouter>, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 })
